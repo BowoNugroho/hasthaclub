285a318d251843228c22879ffe834322ca452f33
BowoNugroho <bowotkj10@gmail.com>
2024-11-29 13:11:57 +0600

    add module admin

---
+++ Modules/Admin/vite.config.js
@@ -0,0 +1,47 @@
+const dotenvExpand = require("dotenv-expand");
+dotenvExpand(
+    require("dotenv").config({ path: "../../.env" /*, debug: true*/ })
+);
+
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+    build: {
+        outDir: "../../public/build-admin",
+        emptyOutDir: true,
+        manifest: true,
+    },
+    plugins: [
+        laravel({
+            publicDirectory: "../../public",
+            buildDirectory: "build-admin",
+            input: [
+                __dirname + "/Resources/assets/sass/app.scss",
+                __dirname + "/Resources/assets/css/app.css",
+                __dirname + "/Resources/assets/js/app.js",
+            ],
+            refresh: true,
+        }),
+    ],
+    css: {
+        postcss: {
+            plugins: [tailwindcss(), require("autoprefixer")],
+        },
+    },
+    resolve: {
+        alias: {
+            "@": "/resources", // Aliases folder `resources/` untuk memudahkan pengelolaan path
+            "@modules": path.resolve(__dirname, "Modules"),
+        },
+    },
+    build: {
+        outDir: "public", // Tentukan folder output build
+        assetsDir: "assets", // Tentukan direktori aset dalam output
+    },
+    // server: {
+    //     host: "0.0.0.0", // Membuka akses dari luar localhost
+    //     port: 5173, // Port yang digunakan
+    //     open: true, // Opsional, ini membuka browser secara otomatis
+    // },
+});
