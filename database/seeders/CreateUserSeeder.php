df26cd981aa59567b9f293abc02b7caefca027b8
BowoNugroho <bowotkj10@gmail.com>
2024-12-03 08:18:52 +0600

    fix (table user) : add kolom no_hp

--- database/seeders/CreateUserSeeder.php
+++ database/seeders/CreateUserSeeder.php
@@ -16,34 +16,36 @@ class CreateUserSeeder extends Seeder
     public function run(): void
     {
         $user = User::create([
-            'name' => 'Admin Hastha Club', 
+            'name' => 'Admin Hastha Club',
             'username' => 'admin',
             'email' => 'admin@hasthaclub.com',
+            'no_hp' => '085487965124',
             'password' => bcrypt('Bismillah'),
-            'status' => true 
+            'status' => true
         ]);
-         
+
         $user->assignRole([1]);
 
         $user2 = User::create([
-            'name' => 'Shop Hastha Club', 
+            'name' => 'Shop Hastha Club',
             'username' => 'shop',
             'email' => 'shop@hasthaclub.com',
+            'no_hp' => '085487465125',
             'password' => bcrypt('Bismillah'),
-            'status' => true 
+            'status' => true
         ]);
-         
+
         $user2->assignRole([2]);
 
         $user3 = User::create([
-            'name' => 'User Hastha Club', 
+            'name' => 'User Hastha Club',
             'username' => 'user',
             'email' => 'user@hasthaclub.com',
+            'no_hp' => '081487965329',
             'password' => bcrypt('Bismillah'),
-            'status' => true 
+            'status' => true
         ]);
-         
-        $user3->assignRole([3]);
 
+        $user3->assignRole([3]);
     }
 }
