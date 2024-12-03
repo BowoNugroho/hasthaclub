e036513b1b1262636e106dca4dd81efdb4df1bd6
BowoNugroho <bowotkj10@gmail.com>
2024-12-03 08:22:23 +0600

    fix route login customer

--- Modules/Shop/Routes/web.php
+++ Modules/Shop/Routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Modules\Shop\Http\Controllers\AuthController;
+use Modules\Shop\Http\Controllers\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,4 +21,13 @@ Route::prefix('/')->group(function () {
 
 Route::prefix('/etalase')->group(function () {
     Route::get('/', 'HomeController@index');
+});
+
+Route::middleware('guest')->group(function () {
+    // Route::get('/login-cs', function () {
+    //     return view('shop::customer.login');
+    // })->name('login-cs');
+    Route::get('/login-cs', [AuthController::class, 'login'])->name('login-cs');
+    Route::post('/login-cs', [AuthController::class, 'actionlogin']);
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
 });
