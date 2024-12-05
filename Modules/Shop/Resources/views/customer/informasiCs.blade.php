d1e45465a2e5a762ae434509b082e00cbad4b9a9
BowoNugroho <bowotkj10@gmail.com>
2024-12-05 08:09:39 +0600

    update data informasi cs

--- Modules/Shop/Resources/views/customer/informasiCs.blade.php
+++ Modules/Shop/Resources/views/customer/informasiCs.blade.php
@@ -7,11 +7,48 @@
             <div class="box col-span-2"></div>
             <div class="box col-span-2">
                 <p class="text-2xl font-bold text-start mb-5 mt-5">Informasi Akun</p>
-                <form action="{{ route('createCs') }}" method="post">
+                <form action="{{ route('updateCs',$dt->id) }}" method="post">
                     @csrf
                     <div class="mb-10 mt-3">
+                        @if(session('success'))
+                        <div id="alert-3" class="flex items-center p-4 mb-4 text-green-800 rounded-lg bg-green-50  role="alert">
+                            <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                            </svg>
+                            <span class="sr-only">Info</span>
+                            <div class="ms-3 text-sm font-medium">
+                                {{ session('success') }}
+                            </div>
+                            <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 " data-dismiss-target="#alert-3" aria-label="Close">
+                              <span class="sr-only">Close</span>
+                              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                              </svg>
+                            </button>
+                        </div>
+                        @endif
+                        @if(session('error'))
+                            <div id="alert-2" class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 " role="alert">
+                                <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                                </svg>
+                                <span class="sr-only">Info</span>
+                                <div class="ms-3 text-sm font-medium">
+                                    {{ session('error') }}
+                                </div>
+                                <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8  " data-dismiss-target="#alert-2" aria-label="Close">
+                                <span class="sr-only">Close</span>
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>
+                                </button>
+                            </div>
+                        @endif
+                    </div>
+                    <div class="mb-10 mt-3">
                         <div class="relative">
-                        <input type="text" id="name" name="name" class="block  p-3.5 bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none w-full  peer @error('name') is-invalid @enderror" placeholder="  " value="{{ old('name') }}" >
+                        {{-- <input type="hidden" name="id" value="{{ $dt->id }}"> --}}
+                        <input type="text" id="name" name="name" class="block  p-3.5 bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none w-full  peer @error('name') is-invalid @enderror" placeholder="  " value="{{ $dt->name }}" >
                         <label for="name" 
                             class="absolute text-sm text-gray-500 duration-300 transform -translate-y-3.5 scale-75 top-3.5 left-3 origin-[0] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:translate-y-[-0.8rem] peer-focus:scale-75 peer-focus:text-blue-600">Nama</label>
                             @if ($errors->has('name'))
@@ -21,7 +58,7 @@
                     </div>
                     <div class="mb-10 mt-3">
                         <div class="relative">
-                        <input type="text" id="no_hp" name="no_hp" class="block  p-3.5 bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none w-full  peer @error('no_hp') is-invalid @enderror" placeholder=" " value="{{ old('no_hp') }}" >
+                        <input type="text" id="no_hp" name="no_hp" class="block  p-3.5 bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none w-full  peer @error('no_hp') is-invalid @enderror" placeholder=" " value="{{ $dt->no_hp }}" >
                         <label for="no_hp" 
                             class="absolute text-sm text-gray-500 duration-300 transform -translate-y-3.5 scale-75 top-3.5 left-3 origin-[0] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:translate-y-[-0.8rem] peer-focus:scale-75 peer-focus:text-blue-600">No. handphone</label>
                             @if ($errors->has('no_hp'))
@@ -31,7 +68,7 @@
                     </div>
                     <div class="mb-10 mt-3">
                         <div class="relative">
-                        <input type="text" id="email" name="email" class="block  p-3.5 bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none w-full  peer @error('email') is-invalid @enderror" placeholder=" " value="{{ old('email') }}" >
+                        <input type="text" id="email" name="email" class="block  p-3.5 bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none w-full  peer @error('email') is-invalid @enderror" placeholder=" " value="{{ $dt->email }}" >
                         <label for="email" 
                             class="absolute text-sm text-gray-500 duration-300 transform -translate-y-3.5 scale-75 top-3.5 left-3 origin-[0] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:translate-y-[-0.8rem] peer-focus:scale-75 peer-focus:text-blue-600">Email</label>
                             @if ($errors->has('email'))
@@ -41,7 +78,7 @@
                     </div>
                     <div class="mb-10 mt-3">
                         <div class="relative">
-                        <input type="text" id="ktp" name="ktp" class="block  p-3.5 bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none w-full  peer @error('ktp') is-invalid @enderror" placeholder=" " value="{{ old('ktp') }}" >
+                        <input type="text" id="ktp" name="ktp" class="block  p-3.5 bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none w-full  peer @error('ktp') is-invalid @enderror" placeholder=" " value="{{ $dt->username }}" >
                         <label for="ktp" 
                             class="absolute text-sm text-gray-500 duration-300 transform -translate-y-3.5 scale-75 top-3.5 left-3 origin-[0] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:translate-y-[-0.8rem] peer-focus:scale-75 peer-focus:text-blue-600">KTP / SIM</label>
                             @if ($errors->has('ktp'))
@@ -51,15 +88,15 @@
                     </div>
                     <div class="mb-10 mt-3">
                         <div class="relative">
-                            <form class="max-w-sm mx-auto">
-                                <label for="gender" 
-                            class="absolute text-sm text-gray-500 duration-300 transform -translate-y-3.5 scale-75 top-3.5 left-3 origin-[0] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:translate-y-[-0.8rem] peer-focus:scale-75 peer-focus:text-blue-600">Jenis Kelamin</label>
-                                <select id="gender" name="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none w-full p-3.5  peer" placeholder=" ">
-                                  <option value="">- pilih -</option>
-                                  <option value="laki-laki">Laki - Laki</option>
-                                  <option value="perempuan">Perempuan</option>
-                                </select>
-                              </form>
+                                <div class="max-w-sm mx-auto">
+                                    <label for="gender" 
+                                    class="absolute text-sm text-gray-500 duration-300 transform -translate-y-3.5 scale-75 top-3.5 left-3 origin-[0] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:translate-y-[-0.8rem] peer-focus:scale-75 peer-focus:text-blue-600">Jenis Kelamin</label>
+                                    <select id="gender" name="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none w-full p-3.5  peer" placeholder=" ">
+                                        <option value="">- pilih -</option>
+                                        <option value="laki-laki" {{ $dt->gender == "laki-laki" ? 'selected' : '' }}>Laki - Laki</option>
+                                        <option value="perempuan" {{ $dt->gender == "perempuan" ? 'selected' : '' }}>Perempuan</option>
+                                    </select>
+                                </div>
                         </div>
                     </div>
                     <div class="mb-10 text-center mt-10">
--- Modules/Shop/Routes/web.php
+++ Modules/Shop/Routes/web.php
@@ -21,7 +21,7 @@ Route::group(['middleware' => ['auth:customer']], function () {
     // Route::group(['middleware' => ['role:shop']], function () {
     Route::prefix('shop')->group(function () {
         Route::get('/', [ShopController::class, 'index'])->name('shop.index');
-        Route::get('/loginCs', [CustomerAuthController::class, 'loginCs'])->name('loginCs');
+        // Route::get('/loginCs', [CustomerAuthController::class, 'loginCs'])->name('loginCs');
     });
     // });
 });
@@ -43,7 +43,7 @@ Route::controller(CustomerAuthController::class)->group(function () {
 
 Route::controller(DashboardController::class)->group(function () {
     Route::get('/dashboardCs', 'index')->name('dashboardCs')->middleware('auth:customer');
-    // Route::get('/dashboardCs', [DashboardController::class, 'index'])->name('dashboardCs')->middleware('auth');
+    Route::post('/updateCs/{id}', 'update')->name('updateCs')->middleware('auth:customer');
 });
 
 Route::controller(RiwayatController::class)->group(function () {
