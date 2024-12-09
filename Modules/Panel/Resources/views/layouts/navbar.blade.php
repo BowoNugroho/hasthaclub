a50a960e9a4b584b77b0c3feba21adadf31421d7
BowoNugroho <bowotkj10@gmail.com>
2024-12-09 15:46:48 +0700

    navbar

--- Modules/Panel/Resources/views/layouts/navbar.blade.php
+++ Modules/Panel/Resources/views/layouts/navbar.blade.php
@@ -110,11 +110,27 @@
                         </a>
                         <div class="dropdown-menu">
                           <a href="#" class="dropdown-item">
-                            Master Data Produk
+                            Master Data Kategori
                           </a>
                           <a href="#" class="dropdown-item">
-                            Master Kategori Produk
+                            Master Data Brand
                           </a>
+                          <a href="#" class="dropdown-item">
+                            Master  Data Produk
+                          </a>
+                          <div class="dropend">
+                            <a href="#" class="dropdown-item dropdown-toggle" href="#sidebar-authentication" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false">
+                              Master  Data Varian
+                            </a>
+                            <div class="dropdown-menu">
+                              <a href="#" class="dropdown-item">
+                                Master  Data Warna
+                              </a>
+                              <a href="#" class="dropdown-item">
+                                Master  Data Kapasitas
+                              </a>
+                            </div>
+                          </div>
                         </div>
                       </div>
                       <div class="dropend">
@@ -131,7 +147,7 @@
                   </div>
                 </div>
             </li>
-            <li class="nav-item dropdown">
+            {{-- <li class="nav-item dropdown">
                 <a class="nav-link " href="#navbar-base" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false" >
                   <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5" /><path d="M12 12l8 -4.5" /><path d="M12 12l0 9" /><path d="M12 12l-8 -4.5" /><path d="M16 5.25l-8 4.5" /></svg>
@@ -140,7 +156,7 @@
                     Data Reseller
                   </span>
                 </a>
-            </li>
+            </li> --}}
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false" >
                   <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
