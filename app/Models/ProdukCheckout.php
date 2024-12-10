c2054894f2c2bb958d68deadafd288ec77e7b2be
BowoNugroho <bowotkj10@gmail.com>
2024-12-10 14:12:45 +0700

    create tabel view best seller

---
+++ app/Models/ProdukCheckout.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Blameable;
+use App\Traits\UuidTraits;
+use Illuminate\Support\Facades\DB;
+
+class ProdukCheckout extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable, UuidTraits, HasRoles, Blameable, SoftDeletes;
+    protected $table = 'produk_checkouts';
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = (string) \Illuminate\Support\Str::uuid();
+        });
+    }
+
+    protected $fillable = [
+        'product_variant_id',
+        'status',
+    ];
+}
---
+++ database/migrations/2024_12_10_065652_create_product_best_sellers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_best_sellers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('product_variant_id');
+            $table->boolean('status')->default(false);
+            $table->uuid('created_by')->nullable();
+            $table->uuid('updated_by')->nullable();
+            $table->uuid('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produk_best_sellers');
+    }
+};
